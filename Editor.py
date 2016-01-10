4caec2074725950bd889b93cbbf312fb935f62d2
Tyler Ishikawa <tyler.ishikawa@outlook.com>
2016-01-10 18:52:15 -0500

    fits opened image to panel

--- Editor.py
+++ Editor.py
@@ -7,13 +7,12 @@ class EditorWindow(wx.Frame):
 
         self.mainPanel = wx.Panel(self)
         self.buttonPanel = wx.Panel(self.mainPanel)
-        self.displayPanel = wx.Panel(self.mainPanel, style=wx.BORDER)
-        self.displayPanel.SetScrollbar(wx.VERTICAL, 0, 5, 10)
+        self.displayPanel = wx.Panel(self.mainPanel, style=wx.SUNKEN_BORDER)
+        #self.displayPanel.SetScrollbar(wx.VERTICAL, 0, 5, 10)
 
         self.button = wx.Button(self.buttonPanel, label="Select a file")
         self.Bind(wx.EVT_BUTTON, self.OnOpen, self.button)
 
-        img = wx.EmptyImage(500, 500)
         self.imageDisplay = wx.StaticBitmap(self.displayPanel, wx.ID_ANY)
 
         self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -34,19 +33,9 @@ class EditorWindow(wx.Frame):
             image = wx.Image(imageFile, wx.BITMAP_TYPE_ANY)
 
             # scale image
-            maxSize = 500
-            width = image.GetWidth()
-            height = image.GetHeight()
-
-            if width > maxSize or height > maxSize:
-                if width > height:
-                    newWidth = 500
-                    newHeight = height / (width / 500.0)
-                else:
-                    newHeight = 500
-                    newWidth = width / (height / 500.0)
-
-                image = image.Scale(newWidth, newHeight)
+            newWidth = self.displayPanel.GetClientSize().GetWidth()
+            newHeight = image.GetHeight() / (image.GetWidth() / float(newWidth))
+            image = image.Scale(newWidth, newHeight)
 
             # Change the displayed image
             self.imageDisplay.SetBitmap(wx.BitmapFromImage(image))
@@ -54,6 +43,6 @@ class EditorWindow(wx.Frame):
         dlg.Destroy()
 
 app = wx.App(False)
-frame = EditorWindow(title="PDFEditor")
+frame = EditorWindow(title="PDFEditor", size=(500,500))
 frame.Show()
 app.MainLoop()
